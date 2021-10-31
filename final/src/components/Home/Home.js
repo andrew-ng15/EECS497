3985e5f713d19c849d0945544f67f3ca7e6b40da
karminga <karminga@umich.edu>
2021-10-31 17:54:21 -0400

    setting up homepage

---
+++ final/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Home.module.scss';
+
+const Home = () => {
+
+    return (
+        <React.Fragment>
+            hello world
+        </React.Fragment>
+    );
+};
+
+export default Home;
