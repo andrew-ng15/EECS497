a532eaa06d420a38441d4bf5196e6a245039061c
teefang <teefang@umich.edu>
2021-11-02 17:56:12 -0400

    add pictures to search

--- final/src/components/Search/Search.js
+++ final/src/components/Search/Search.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import classes from './Search.module.scss';
+import profpic from './uploads/profpic.jpg';
+import logo from './uploads/logo.jpg';
+import search from './uploads/search.png'
 
 const Search = () => {
 
 
     return (
-        <div>
-            <img src="logo.jpg" className={classes.Logo} />
+        <div className={classes.Search}>
+            <img src={profpic} className={classes.Logo} />
             <div className={classes.Profile}>
-                <img src="profpic.jpg" />
+                <img src={logo} />
                 <h3>mockuser</h3>
             </div>
             <div className={classes.SearchBar}>
-                <img src="searchimage.jpg"/>
+                <img src={search} />
                 <form>
                     <input className={classes.Text} type="text" placeholder="Search For Events" />
                     <input className={classes.Submit} type="submit" value="search" />
